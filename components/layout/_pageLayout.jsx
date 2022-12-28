c6e2df23efe2ec64983cdc70728f149cf1ce7a22
David Nevarez <david.nevarez03@hotmail.com>
2022-12-28 02:25:49 -0700

    removed "layoutStyles"

--- components/layout/_pageLayout.jsx
+++ components/layout/_pageLayout.jsx
@@ -5,7 +5,7 @@ import Navbar from "./../../components/layout/navbars/_navbarLayout";
 import Footer from "./../../components/layout/footers/_footerLayout";
 
 // styles
-import layoutStyles from "./../../styles/components/layout/_pagelayout.module.scss";
+// import layoutStyles from "./../../styles/components/layout/_pagelayout.module.scss";
 
 // global fonts
 import { Inter } from "@next/font/google";
@@ -13,7 +13,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 const PageLayout = ({ children }) => {
   return (
-    <div id={layoutStyles.page_container} className={inter.className}>
+    <div className={inter.className}>
       {/* <Navbar /> */}
       {children}
       {/* <Footer /> */}
